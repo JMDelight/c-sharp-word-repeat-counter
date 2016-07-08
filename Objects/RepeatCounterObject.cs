b1b953298794d5388169ba077101cac48eed676a
Joel Delight <thefencingflutist@gmail.com>
2016-07-08 11:32:22 -0700

    add getters and setters for RepeatCounter.

--- Objects/RepeatCounterObject.cs
+++ Objects/RepeatCounterObject.cs
@@ -7,10 +7,32 @@ namespace WordCounter.Objects
     private string _userSentence;
     private string _wordToCheck;
 
+
     public RepeatCounter(string userSentence, string wordToCheck)
     {
       _userSentence = userSentence;
       _wordToCheck = wordToCheck;
+    }
+
+    // -- Setters for RepeatCounter -- //
+    public void SetUserSentence(string newSentence)
+    {
+      _userSentence = newSentence;
+    }
+    public void SetWordToCheck(string newWord)
+    {
+      _wordToCheck = newWord;
+    }
+
+    // -- Getters for RepeatCounter -- //
+    public string GetUserSentence()
+    {
+      return _userSentence;
+    }
+
+    public string GetWordToCheck()
+    {
+      return _wordToCheck;
     }
 
     // -- A function that takes an inputted sentence and counts how many instances of the inputted word are contained in the snetence -- //
@@ -23,7 +45,6 @@ namespace WordCounter.Objects
       string[] sentenceWords = sentence.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
       foreach (string checkedWord in sentenceWords)
       {
-        // System.Console.WriteLine(checkedWord);
         if (checkedWord == word)
         {
           count ++;
